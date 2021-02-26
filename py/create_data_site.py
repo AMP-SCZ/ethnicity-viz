792b66ae28b35f91cd9ee4d21dba3408d5b52996
Tashrif Billah <tashrifbillah@gmail.com>
2021-02-26 15:55:10 -0500

    create site data

--- py/create_data_site.py
+++ py/create_data_site.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from datetime import date, timedelta
 
-start_date= date(2019,12,1)
+start_date= date(2019,11,1)
 end_date= date(2021,2,24)
 delta= timedelta(days=2)
 
@@ -14,14 +14,32 @@ sex=['Female','Male','Unknown/Not Reported']
 wellness=['Patient','Healthy']
 
 races_prob= [0.20,0.50,0.25,0.05]
+races_prob= [0.10,0.70,0.15,0.05]
+races_prob= [0.10,0.60,0.05,0.25]
+# races_prob= [0.10,0.70,0.10,0.10]
+
 eths_prob= [0.15,0.75,0.10]
+eths_prob= [0.10,0.75,0.15]
+eths_prob= [0.05,0.85,0.10]
+# eths_prob= [0.05,0.80,0.15]
+
 sex_prob= [0.60,0.35,0.05]
+sex_prob= [0.20,0.70,0.10]
+sex_prob= [0.50,0.50,0.00]
+# sex_prob= [0.35,0.65,0.00]
+
 wellness_prob= [0.8,0.2]
+# wellness_prob= [0.90,0.10]
 
-site='MGH'
+outfile= f'../data/ProNET/MGH_metadata.csv'
+outfie= f'../data/ProNET/BWH_metadata.csv'
+outfie= f'../data/ProNET/Yale_metadata.csv'
+
+outfie= f'../data/PRESCIENT/Adelaide_metadata.csv'
+outfie= f'../data/PRESCIENT/Melbourne_metadata.csv'
+outfie= f'../data/PRESCIENT/Sydney_metadata.csv'
 
 
-# actual
 df= pd.DataFrame(columns=['Consent Date','Race','Ethnicity','Sex','Wellness'])
 k=0
 i= start_date
@@ -40,6 +58,6 @@ while i<=end_date:
         
         k+=1
 
-df.to_csv(f'../data/PRESCIENT/{site}_metadata.csv', index=False)
+df.to_csv(outfile, index=False)
 
 
