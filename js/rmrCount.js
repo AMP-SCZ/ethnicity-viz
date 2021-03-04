d35a5edbe3005eb3442bb0b271b62ce1d0f20788
Tashrif Billah <tashrifbillah@gmail.com>
2021-03-04 09:54:19 -0500

    interfacing with rmrVis() standard viz class wip

--- js/rmrCount.js
+++ js/rmrCount.js
@@ -81,7 +81,7 @@ function filterByDate(siteData) {
 
 
 let total, totalHisp, totalMinor
-function rmrCount(data) {
+function rmrActualCount(data) {
     
     total= new Array(7).fill(0)
     totalHisp= new Array(7).fill(0)
@@ -155,6 +155,49 @@ function rmrCount(data) {
 
 today= new Date().getTime()
 today= d3.timeParse("%m/%d/%Y")("4/1/2021")
+function rmrPlanCount(planData) {
+    let totalTarget= new Array(7).fill(0)
+    let totalMinorTarget= new Array(7).fill(0)
+    let totalHispTarget= new Array(7).fill(0)
+       
+    let currTarget
+    
+    planData.forEach(data => {
+        
+        currTarget= Object.values(data[0]).filter((d,i)=>i>0).map(d=>+d)
+        currTarget.forEach((d,i)=> totalTarget[i]+=d)
+        
+        currTarget= Object.values(data[4]).filter((d,i)=>i>0).map(d=>+d)
+        currTarget.forEach((d,i)=> totalMinorTarget[i]+=d)
+        
+        currTarget= Object.values(data[8]).filter((d,i)=>i>0).map(d=>+d)
+        currTarget.forEach((d,i)=> totalHispTarget[i]+=d)
+        
+        
+        table= document.getElementById('rmr-report')
+        
+        report_dates.forEach((d,i)=> {
+            if (totalTarget[i]) {
+                // total target
+                table.rows[1].cells[i+1].innerText=d3.format(',')(totalTarget[i])
+                table.rows[4].cells[i+1].innerText=d3.format(',')(totalMinorTarget[i])
+                table.rows[7].cells[i+1].innerText=d3.format(',')(totalHispTarget[i])
+                
+                // actual/target ratio
+                if  (d<=today) {
+                    table.rows[3].cells[i+1].innerText=calcRatio(total[i], totalTarget[i])
+                    table.rows[6].cells[i+1].innerText=calcRatio(totalMinor[i], totalMinorTarget[i])
+                    table.rows[9].cells[i+1].innerText=calcRatio(totalHisp[i], totalHispTarget[i])
+                }
+                
+            }
+        })
+    
+    })
+    
+}
+
+/*
 function loadRmrData(filePrefixes) {
     let totalTarget= new Array(7).fill(0)
     let totalMinorTarget= new Array(7).fill(0)
@@ -197,11 +240,12 @@ function loadRmrData(filePrefixes) {
                     table.rows[4].cells[i+1].innerText=d3.format(',')(totalTarget[i])
                     table.rows[8].cells[i+1].innerText=d3.format(',')(totalMinorTarget[i])
                     table.rows[12].cells[i+1].innerText=d3.format(',')(totalHispTarget[i])
-                    */
+                    
                 }
             })
         
         })
     })
 }
+*/
 
